ccff311139c5e3027451f1cf41eb75ff2fcfb200
hahaxixi <he.huang@extremevision.mo>
2017-12-14 17:46:18 +0800

    update face tests

--- tests/FacePlus/Body/BodyTest.php
+++ tests/FacePlus/Body/BodyTest.php
@@ -16,9 +16,9 @@ class BodyTest extends \PHPUnit_Framework_TestCase
     public function testGet()
     {
         $config = require __DIR__ . '/../../config/face_plus.php';
-        $result = Entry::Body($config)->face->select('detect')->where(['image_file' =>__DIR__ . '/../../file/face_01.jpg' , 'return_attributes' => 'skinstatus'])->get();//身份证
-//        $result = Entry::Body($config)->face->select('detect')->where(['image_base64' => \AI\Common\Tool\File::base64LocalImage(__DIR__ . '/../../file/face_01.jpg'), 'return_attributes' => 'skinstatus'])->get();//身份证
-//        $result = Entry::Body($config)->face->select('detect')->where(['image_url' => 'http://open.youtu.qq.com/app/img/experience/face_img/face_06.jpg', 'return_attributes' => 'skinstatus'])->get();//身份证
+        $result = Entry::FacePlus($config)->body->select('gesture')->where(['image_file' =>__DIR__ . '/../../file/Gesture6.jpg' , 'return_gesture' => '1'])->get();//Gesture
+//        $result = Entry::FacePlus($config)->body->select('gesture')->where(['image_base64' => \AI\Common\Tool\File::base64LocalImage(__DIR__ . '/../../file/face_01.jpg'), 'return_attributes' => 'skinstatus'])->get();//Gesture
+//        $result = Entry::FacePlus($config)->body->select('gesture')->where(['image_url' => 'http://open.youtu.qq.com/app/img/experience/face_img/face_06.jpg', 'return_attributes' => 'skinstatus'])->get();//Gesture
         var_dump($result);
         exit;
     }
--- tests/FacePlus/Image/ImageTest.php
+++ tests/FacePlus/Image/ImageTest.php
@@ -16,9 +16,9 @@ class ImageTest extends \PHPUnit_Framework_TestCase
     public function testGet()
     {
         $config = require __DIR__ . '/../../config/face_plus.php';
-        $result = Entry::Image($config)->face->select('detect')->where(['image_file' =>__DIR__ . '/../../file/face_01.jpg' , 'return_attributes' => 'skinstatus'])->get();//身份证
-//        $result = Entry::Image($config)->face->select('detect')->where(['image_base64' => \AI\Common\Tool\File::base64LocalImage(__DIR__ . '/../../file/face_01.jpg'), 'return_attributes' => 'skinstatus'])->get();//身份证
-//        $result = Entry::Image($config)->face->select('detect')->where(['image_url' => 'http://open.youtu.qq.com/app/img/experience/face_img/face_06.jpg', 'return_attributes' => 'skinstatus'])->get();//身份证
+        $result = Entry::FacePlus($config)->image->select('detect')->where(['image_file' =>__DIR__ . '/../../file/face_01.jpg' , 'return_attributes' => 'skinstatus'])->get();//身份证
+//        $result = Entry::FacePlus($config)->image->select('detect')->where(['image_base64' => \AI\Common\Tool\File::base64LocalImage(__DIR__ . '/../../file/face_01.jpg'), 'return_attributes' => 'skinstatus'])->get();//身份证
+//        $result = Entry::FacePlus($config)->image->select('detect')->where(['image_url' => 'http://open.youtu.qq.com/app/img/experience/face_img/face_06.jpg', 'return_attributes' => 'skinstatus'])->get();//身份证
         var_dump($result);
         exit;
     }
