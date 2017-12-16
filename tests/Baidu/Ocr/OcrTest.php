301544a298dc0c7125d33929326d49f55f434e7f
hahaxixi <he.huang@extremevision.mo>
2017-12-16 16:22:57 +0800

    change vendor name

--- tests/Baidu/Ocr/OcrTest.php
+++ tests/Baidu/Ocr/OcrTest.php
@@ -10,10 +10,13 @@ use AI\Entry;
  */
 class OcrTest extends \PHPUnit_Framework_TestCase
 {
+    //vendor/phpunit/phpunit/phpunit --testdox tests/Baidu/Ocr/OcrTest.php
     public function testGet()
     {
         $config = require __DIR__.'/../../config/ai.php';
         $result = Entry::Baidu($config)->ocr->select('idcard')->where(['image' => file_get_contents(__DIR__ . '/../../file/idcard.jpg'), 'id_card_side' => 'front'])->get();//身份证
+        var_dump($result);exit;
+        return $result;
 //        $result = Entry::Baidu($config)->ocr->select('receipt')->where(['image' => file_get_contents(__DIR__ . '/../../file/demo-receipt-1.jpg'), 'id_card_side' => 'front'])->get();//票据
 //        $result = Entry::Baidu($config)->ocr->select('form_ocr/request')->where(['image' => file_get_contents(__DIR__ . '/../../file/idcard.jpg')])->get();//身份证
 //        $result = Entry::Baidu($config)->ocr->select('vehicle_license')->where(['image' => file_get_contents(__DIR__ . '/../../file/xingshizheng002.jpg')])->get();//行驶证
